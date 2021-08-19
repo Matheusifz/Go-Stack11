c22e38a79d6dde6fd4bdf0a82b40e524169f2b44
matheusifz <matheusifz@gmail.com>
2021-08-19 16:26:57 -0300

    feat: integrate backend and frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import "./Style.css";
 import Header from "./components/Header";
 
 function App() {
-  const [projects, setProjects] = useState([
-    "Desenvolvimeno de App",
-    "Front-end web",
-  ]);
+  const [projects, setProjects] = useState([]);
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
-    console.log(projects);
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    //setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    const response = await api.post("projects", {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: "MATHEUS IGOR",
+    });
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
     <>
       <Header title="Projects" />
+
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
       <button type="button" onClick={handleAddProject}>
